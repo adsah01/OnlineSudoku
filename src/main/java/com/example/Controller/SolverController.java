26454065a9f76487a9f35d03a91d4f5b1ec4e2dc
adsah01 <adsah01@gmail.com>
2017-05-17 14:55:56 +0200

    Designfix

--- src/main/java/com/example/Controller/SolverController.java
+++ src/main/java/com/example/Controller/SolverController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.validation.Valid;
 
 
-/**
- * Styr upp annoteringar sen!!
- */
 @Controller
 public class SolverController {
 
